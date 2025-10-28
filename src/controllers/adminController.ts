a475e481cab9e8875ba3ba8e9bcc2d4c9f40518b
NexusProgrammers <jawadprogrammer@gmail.com>
2025-10-28 12:45:37 -0700

    clean

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import { adminUserService } from "../services/index";
-import { uploadSingleImage } from "../middlewares/upload";
 
 export const registerAdminUser = async (req: Request, res: Response): Promise<void> => {
   try {
